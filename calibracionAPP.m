e352af376e4e53c8ca830e4ed85177b8c7fe4e65
Juan <jumasaet@espol.edu.ec>
2025-07-14 08:46:50 -0500

    Testing calibration

--- calibracionAPP.m
+++ calibracionAPP.m
@@ -1,6 +1,7 @@
-I1 = imread('C:/StereoThermal/captures/inverse/thermal_20250710_110814.png');
-I2 = imread('C:/StereoThermal/gray.png');
+I1 = imread('C:\StereoThermal\captures\visible\left\LEFT_visible_20250710_110814.png');
+I2 = imread('C:\StereoThermal\captures\visible\right\RIGHT_visible_20250710_110814.png');
 
 % Ejemplo: Rectificar y visualizar un par de imágenes
-[J1, J2] = rectifyStereoImages(I1, I2, cam_params);
+[J1, J2] = rectifyStereoImages(I2, I1, cam_params);
 figure; imshow(stereoAnaglyph(J1, J2)); title('Imágenes rectificadas (anaglifo)');
+
