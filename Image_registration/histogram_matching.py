348e6b4966cd49ceae8fa09ab7ae8b6d85c825e3
Nick Arévalo <69706202+Nick451109@users.noreply.github.com>
2025-08-08 12:13:40 -0500

    revert

--- Image_registration/histogram_matching.py
+++ Image_registration/histogram_matching.py
@@ -4,12 +4,13 @@ from skimage.exposure import match_histograms
 import matplotlib.pyplot as plt
 import os
 
-def histogram_matching_visible_a_termica(
+def histogram_matching_termica_a_visible(
     ruta_termica,
     ruta_visible,
-    ruta_salida="salidas/visible_matched.png",
+    ruta_salida="salidas/termica_matched.png",
     mostrar=True
 ):
+    # Leer imágenes en escala de grises
     termica = cv2.imread(ruta_termica, cv2.IMREAD_GRAYSCALE)
     visible = cv2.imread(ruta_visible, cv2.IMREAD_GRAYSCALE)
 
@@ -17,12 +18,14 @@ def histogram_matching_visible_a_termica(
         raise FileNotFoundError("No se pudo cargar una de las imágenes.")
 
     # Aplicar histogram matching: Térmica → Visible
-    visible_ajustada = match_histograms(visible, termica).astype(np.uint8)
+    termica_ajustada = match_histograms(termica, visible).astype(np.uint8)
 
+    # Guardar resultado
     os.makedirs(os.path.dirname(ruta_salida), exist_ok=True)
-    cv2.imwrite(ruta_salida, visible_ajustada)
+    cv2.imwrite(ruta_salida, termica_ajustada)
     print(f"[INFO] Imagen térmica ajustada guardada en: {ruta_salida}")
 
+    # Mostrar comparación si se desea
     if mostrar:
         plt.figure(figsize=(12, 4))
         plt.subplot(1, 3, 1)
@@ -34,16 +37,16 @@ def histogram_matching_visible_a_termica(
         plt.title("Visible (referencia)")
 
         plt.subplot(1, 3, 3)
-        plt.imshow(visible_ajustada, cmap="gray")
-        plt.title("Visible ajustada")
+        plt.imshow(termica_ajustada, cmap="gray")
+        plt.title("Térmica ajustada")
         plt.tight_layout()
         plt.show()
 
-    return visible_ajustada
+    return termica_ajustada
 
 # === EJEMPLO DE USO ===
 ruta_termica = "captures/inverse/thermal_20250710_110814.png"
 ruta_visible = "captures/visible/right/RIGHT_visible_20250710_110814.png"
 ruta_salida = "histogram_matching/visible_matched.png"
 
-histogram_matching_visible_a_termica(ruta_termica, ruta_visible, ruta_salida)
+histogram_matching_termica_a_visible(ruta_termica, ruta_visible, ruta_salida)
